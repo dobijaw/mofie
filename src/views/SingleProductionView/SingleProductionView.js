6ec05bf689907541603234051512f3af94077003
Wojciech Dobija <dobijaw@gmail.com>
2020-04-30 21:25:33 +0200

    change SingleProductionView to use useFetch and display shows or movies depending on the type

--- src/views/SingleProductionView/SingleProductionView.js
+++ src/views/SingleProductionView/SingleProductionView.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { useFetch } from 'hooks';
 import styles from './SingleProductionView.module.scss';
 import MovieYear from '../../components/SingleMovie/MovieYear/MovieYear';
 import MovieTitle from '../../components/SingleMovie/MovieTitle/MovieTitle';
@@ -9,40 +10,44 @@ import MoviePoster from '../../components/SingleMovie/MoviePoster/MoviePoster';
 
 const SingleMovieView = (props) => {
   const { id, type } = props.match.params;
-  const [movie, setMovie] = useState({});
 
-  useEffect(() => {
-    const productionType = type === 'movies' ? 'movie' : 'tv';
+  const [data, error, loading] = useFetch(
+    `https://api.themoviedb.org/3/${
+      type === 'movies' ? 'movie' : 'tv'
+    }/${id}?api_key=f0881d0904275b8ecded5ddeaa83fe30&language=en-US`,
+  );
 
-    fetch(
-      `https://api.themoviedb.org/3/${productionType}/${id}?api_key=f0881d0904275b8ecded5ddeaa83fe30&language=en-US`,
-    )
-      .then((res) => res.json())
-      .then((res) => {
-        setMovie({
-          ...movie,
-          title: type === 'movies' ? res.title : res.original_name,
-          posterPath: `http://image.tmdb.org/t/p/w500/${res.poster_path}`,
-          description: res.overview,
-          genres: res.genres.map((g) => g.name),
-          year: res.release_date,
-        });
-      });
-  }, [id, movie, type]);
   return (
     <>
-      <section className={styles.movieWrapper}>
-        <div className={styles.movieWrapperItem}>
-          <MoviePoster img={movie.posterPath} poster />
-        </div>
-        <div className={styles.movieWrapperItem}>
-          <MovieYear year={movie.year} />
-          <MovieTitle title={movie.title} />
-          {movie.genres < 0 && <MovieGenres genres={movie.genres} />}
-          <MovieDescription description={movie.description} />
-        </div>
-      </section>
-      <Comments />
+      {error && 'Something went wrong!'}
+      {loading ? (
+        'Is loading'
+      ) : (
+        <>
+          <section className={styles.movieWrapper}>
+            <div className={styles.movieWrapperItem}>
+              <MoviePoster
+                img={`http://image.tmdb.org/t/p/w500/${data.poster_path}`}
+                poster
+              />
+            </div>
+            <div className={styles.movieWrapperItem}>
+              <MovieYear
+                year={
+                  type === 'movies' ? data.release_date : data.first_air_date
+                }
+              />
+              <MovieTitle
+                title={type === 'movies' ? data.original_title : data.name}
+              />
+              <MovieGenres genres={data.genres.map((i) => i.name)} />
+              <MovieDescription description={data.overview} />
+              {data.adult && 'Only for Adults!'}
+            </div>
+          </section>
+          <Comments />
+        </>
+      )}
     </>
   );
 };
